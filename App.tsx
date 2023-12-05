3c072405e1af97eb1d851f10b803b359e2f2ed94
Chibuike <chibuike.dmn@gmail.com>
2023-12-05 08:43:37 +0000

    feat: creado en app.tsx la importacion y agregacion del login

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import { HomeScreen } from './components/HomeScreen';
+import {} from './components/LoginScreen'
 
 
 const Drawer = createDrawerNavigator();
@@ -10,6 +11,7 @@ export default function App() {
   return (
       <NavigationContainer>
         <Drawer.Navigator initialRouteName="Login">
+          <Drawer.Screen name="Login" component={LoginScreen} />
           <Drawer.Screen name="Home" component={HomeScreen} />
         </Drawer.Navigator>
       </NavigationContainer>
