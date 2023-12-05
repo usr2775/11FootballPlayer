2f6966237b7437611ec8c14e483b94efec0acbf6
Chibuike <chibuike.dmn@gmail.com>
2023-12-05 08:22:43 +0000

    feat: creado autthContext para el login

---
+++ components/AuthContext.tsx
@@ -0,0 +1,35 @@
+import React, { createContext, useState, useContext } from 'react';
+
+interface AuthContextType {
+  user: { username: string } | null;
+  login: (userData: { username: string }) => void;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [user, setUser] = useState<{ username: string } | null>(null);
+
+  const login = (userData: { username: string }) => {
+    setUser(userData);
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = (): AuthContextType => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth debe ser utilizado dentro de un AuthProvider');
+  }
+  return context;
+};
