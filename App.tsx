58243b7a9e9d2da0deaebac6e89fdbc634776d4f
Chibuike <chibuike.dmn@gmail.com>
2023-12-05 08:47:05 +0000

    feat: creado en app.tsx la etiqueta del authprovider

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import LoginScreen from './components/LoginScreen';
 import { HomeScreen } from './components/HomeScreen';
-import {} from './components/LoginScreen'
-
+import { AuthProvider } from './components/AuthContext';
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
+    <AuthProvider>
       <NavigationContainer>
         <Drawer.Navigator initialRouteName="Login">
           <Drawer.Screen name="Login" component={LoginScreen} />
           <Drawer.Screen name="Home" component={HomeScreen} />
         </Drawer.Navigator>
       </NavigationContainer>
-
+    </AuthProvider>
   );
 }
