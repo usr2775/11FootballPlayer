cf56af8567cfccd2b488e2cadb898351221c568c
Chibuike <chibuike.dmn@gmail.com>
2023-12-05 08:51:43 +0000

    feat: creado en el app portafolio

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { StyleSheet } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import LoginScreen from './components/LoginScreen';
+import Portfolio from './components/Portafolio';
 import { HomeScreen } from './components/HomeScreen';
 import { AuthProvider } from './components/AuthContext';
 
@@ -15,6 +16,7 @@ export default function App() {
         <Drawer.Navigator initialRouteName="Login">
           <Drawer.Screen name="Login" component={LoginScreen} />
           <Drawer.Screen name="Home" component={HomeScreen} />
+          <Drawer.Screen name="Portfolio" component={Portfolio} />
         </Drawer.Navigator>
       </NavigationContainer>
     </AuthProvider>
